b839d9edc7eb7b21897fb8614211710598fc3499
matthieu0guim <m.guimard@solaris-bi.com>
2021-10-08 11:25:11 +0200

    dernière modifications

---
+++ brutforce.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import time
+
+
+first_tab = pd.read_csv("data/partie_1.csv", sep=';', encoding='latin-1')
+list_of_actions = [(first_tab.loc[i].values[0], first_tab.loc[i].values[1], round(first_tab.loc[i].values[2] * first_tab.loc[i].values[1], 2)) for i in range(len(first_tab))]
+list_of_profits = [round(action[1] * action[2], 2) for action in list_of_actions]
+
+"""test case"""
+# actions = [('Action_1', 20, 1), ('Action_2', 30, 3), ('Action_3', 50, 7.5), ('Action_4', 70, 14)]
+
+start = time.time()
+
+
+def sac_dos(capacite, elements, element_selection = []):
+    if elements:
+        val1, lstVal1 = sac_dos(capacite, elements[1:], element_selection)
+        val = elements[0]
+        if val[1] <= capacite:
+            val2, lstVal2 = sac_dos(capacite - val[1], elements[1:], element_selection + [val])
+            if val1 < val2:
+                return val2, lstVal2
+        return val1, lstVal1
+    else:
+        return round(sum([i[2] for i in element_selection]), 2), element_selection
+
+a = sac_dos(500, list_of_actions)
+print(a)
+
+print(f"Avec combinaison restaurée, le programme prend {time.time() - start}s")
+
+print(f"investissment de {sum([action[1] for action in a[1]])}")
+
--- optimized.py
+++ optimized.py
@@ -7,14 +7,17 @@ config = []
 
 """big number of data"""
 #todo demander le nom du fichier en ligne de commande
-new_tab = pd.read_csv("data/dataset2_Python+P7.csv", sep=';', encoding='latin-1')
+user_choice = input("Voulez-vous tester le dataset1 ou le dataset2?")
+
+new_tab = pd.read_csv(f"data/{user_choice}_Python+P7.csv", sep=';', encoding='latin-1')
 first_tab = new_tab.loc[new_tab['price'] > 0 | new_tab['name'].isnull() | new_tab['name'].isna()].reset_index()
 list_of_actions = [(first_tab['name'][i], first_tab['price'][i], first_tab['profit'][i]) for i in range(len(first_tab))]
 weights = [action[1]*100 for action in list_of_actions]
 profits = [round(action[1] * action[2]/100, 2) for action in list_of_actions]
 table = np.zeros((len(list_of_actions) + 1, 50001))
 
-
+print(f" nombre de données avec un prix <= 0: {len(new_tab.loc[new_tab['price'] <= 0 | new_tab['name'].isnull() | new_tab['name'].isna()])}")
+exit()
 for i in range(0, len(list_of_actions) + 1):
     for j in range(0, len(table[0])):
         # base case
